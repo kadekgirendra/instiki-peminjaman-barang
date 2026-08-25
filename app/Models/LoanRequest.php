638bb3a5b8761b31f72527b2756c312423e7a206
kadekgirendra <kadekgirendra@gmail.com>
2026-08-25 19:16:55 +0800

    update: loan request model

--- app/Models/LoanRequest.php
+++ app/Models/LoanRequest.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property \Illuminate\Support\Carbon $start_date
+ * @property \Illuminate\Support\Carbon $end_date
+ */
+
 class LoanRequest extends Model
 {
     protected $fillable = ['user_id', 'start_date', 'end_date', 'purpose', 'document_path'];
