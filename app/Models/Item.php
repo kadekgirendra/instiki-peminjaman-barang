0b604d5da2640023938eec30e0070c02485895a9
kadekgirendra <kadekgirendra@gmail.com>
2026-08-27 20:53:42 +0800

    update: item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['name', 'category', 'description', 'image', 'total_stock', 'daily_fine_rate'])]
 class Item extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     public function transactions()
     {
         return $this->hasMany(Transaction::class);
