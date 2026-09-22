1d6acb913fd08a0a5e9791b0fb9e4abfa7d948c5
kadekgirendra <kadekgirendra@gmail.com>
2026-09-22 11:51:37 +0800

    chore: clean duplicate model imports

--- app/Models/LoanRequest.php
+++ app/Models/LoanRequest.php
@@ -7,9 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-
 /**
  * @property Carbon $start_date
  * @property Carbon $end_date
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 /**
  * @property Carbon $start_date
  * @property Carbon $end_date
